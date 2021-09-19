cabc95211e7421d6dfcb4c3b3bf3320753bb79d1
Setiyandy15 <90827169+Setiyandy15@users.noreply.github.com>
2021-09-19 14:02:29 +0700

    Add files via upload

---
+++ phppertamaku.php
@@ -0,0 +1,12 @@
+<?php
+    /* Setiyandy
+    version : 1.0 */
+
+    $nama = "setiyandy";
+    $usia = 20;
+    $berat = 65;
+
+    echo " <b> Biodata Diri </b> <br>";
+    echo "Nama : ".$nama."<br>";
+    echo "Usia : ".$usia."<br>";
+    echo "Berat : ".$berat."<br>";
